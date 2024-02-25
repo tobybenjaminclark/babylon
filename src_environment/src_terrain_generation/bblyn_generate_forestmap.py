fc62fe55a859147be94de6cd1dcd8db021a5fc61
Toby Clark <psytc9@nottingham.ac.uk>
2024-02-25 21:54:19 +0000

    add: python typing to rock and forest generation functions

--- src_environment/src_terrain_generation/bblyn_generate_forestmap.py
+++ src_environment/src_terrain_generation/bblyn_generate_forestmap.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def generate_forestmap(heightmap):
+# Function to generate a forest map of true/false values from a heightmap.
+def generate_forestmap(heightmap) -> np.array[True|False]:
     heightmap_array = np.array(heightmap)
     forestmap = np.zeros_like(heightmap_array, dtype=bool)
 
@@ -10,8 +11,6 @@ def generate_forestmap(heightmap):
 
     for _ in range(num_clusters):
         cluster_center = np.random.randint(0, heightmap_array.shape[0]), np.random.randint(0, heightmap_array.shape[1])
-
-        # Generate forest in the surrounding tiles based on the selected center
         indices = np.array(list(np.ndindex(heightmap_array.shape)))
         distances = np.linalg.norm(indices - np.array(cluster_center), axis=1)
         valid_heights = (heightmap_array[indices[:, 0], indices[:, 1]] > 135) & (heightmap_array[indices[:, 0], indices[:, 1]] < 255)
@@ -19,7 +18,6 @@ def generate_forestmap(heightmap):
         probabilities = np.exp(-distances / cluster_radius) * cluster_density
         probabilities *= valid_heights
 
-        # Use boolean indexing for updating forestmap
         forestmap |= np.random.rand(*forestmap.shape) < probabilities.reshape(forestmap.shape)
 
     return forestmap
--- src_environment/src_terrain_generation/bblyn_generate_rockmap.py
+++ src_environment/src_terrain_generation/bblyn_generate_rockmap.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def generate_rockmap(heightmap):
+# Function to generate a rock map of true/false values from a heightmap.
+def generate_rockmap(heightmap) -> np.array[True|False]:
     heightmap_array = np.array(heightmap)
     rockmap = np.zeros_like(heightmap_array, dtype = bool)
 
@@ -10,8 +11,6 @@ def generate_rockmap(heightmap):
 
     for _ in range(num_clusters):
         cluster_center = np.random.randint(0, heightmap_array.shape[0]), np.random.randint(0, heightmap_array.shape[1])
-
-        # Generate forest in the surrounding tiles based on the selected center
         indices = np.array(list(np.ndindex(heightmap_array.shape)))
         distances = np.linalg.norm(indices - np.array(cluster_center), axis=1)
         valid_heights = (heightmap_array[indices[:, 0], indices[:, 1]] > 85) & (heightmap_array[indices[:, 0], indices[:, 1]] < 99)
@@ -19,7 +18,6 @@ def generate_rockmap(heightmap):
         probabilities = np.exp(-distances / cluster_radius) * cluster_density
         probabilities *= valid_heights
 
-        # Use boolean indexing for updating forestmap
         rockmap |= np.random.rand(*rockmap.shape) < probabilities.reshape(rockmap.shape)
 
     return rockmap
